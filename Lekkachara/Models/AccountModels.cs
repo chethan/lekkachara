07b178801f96199863bc614820531671d6516e8e
unknown <Administrator@.(none)>
2011-02-02 23:11:41 +0530

    Adding login page

--- Lekkachara/Models/AccountModels.cs
+++ Lekkachara/Models/AccountModels.cs
@@ -86,23 +86,12 @@ namespace Lekkachara.Models
 
     public class AccountMembershipService : IMembershipService
     {
-        private readonly MembershipProvider _provider;
-
-        public AccountMembershipService()
-            : this(null)
-        {
-        }
-
-        public AccountMembershipService(MembershipProvider provider)
-        {
-            _provider = provider ?? Membership.Provider;
-        }
 
         public int MinPasswordLength
         {
             get
             {
-                return _provider.MinRequiredPasswordLength;
+                return 8;
             }
         }
 
@@ -111,7 +100,7 @@ namespace Lekkachara.Models
             if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
             if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
 
-            return _provider.ValidateUser(userName, password);
+            return userName=="undesh" && password=="heavyundesh";
         }
 
         public MembershipCreateStatus CreateUser(string userName, string password, string email)
@@ -120,9 +109,7 @@ namespace Lekkachara.Models
             if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
             if (String.IsNullOrEmpty(email)) throw new ArgumentException("Value cannot be null or empty.", "email");
 
-            MembershipCreateStatus status;
-            _provider.CreateUser(userName, password, email, null, null, true, null, out status);
-            return status;
+            return MembershipCreateStatus.Success;
         }
 
         public bool ChangePassword(string userName, string oldPassword, string newPassword)
@@ -130,22 +117,7 @@ namespace Lekkachara.Models
             if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
             if (String.IsNullOrEmpty(oldPassword)) throw new ArgumentException("Value cannot be null or empty.", "oldPassword");
             if (String.IsNullOrEmpty(newPassword)) throw new ArgumentException("Value cannot be null or empty.", "newPassword");
-
-            // The underlying ChangePassword() will throw an exception rather
-            // than return false in certain failure scenarios.
-            try
-            {
-                MembershipUser currentUser = _provider.GetUser(userName, true /* userIsOnline */);
-                return currentUser.ChangePassword(oldPassword, newPassword);
-            }
-            catch (ArgumentException)
-            {
-                return false;
-            }
-            catch (MembershipPasswordException)
-            {
-                return false;
-            }
+            return true;
         }
     }
 
